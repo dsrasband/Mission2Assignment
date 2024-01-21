b89fdd58a691fd8c25e96ee39c3ca1eed055d9e8
davisrasband <davisrasband@DAVISRASBAN9710>
2024-01-20 23:15:12 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2Assignment
+{
+    internal class Class1
+    {
+        public void RollDice(int number)
+        {
+            Random random = new Random();
+
+            // Initialize an array to hold the roll results
+            int[] rolls = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            for (int iCount = 1; iCount <= number; iCount++) 
+            {
+
+                // Generate random numbers for each die
+                int die1 = random.Next(1, 7);
+                int die2 = random.Next(1, 7);
+
+                // Add the two dice rolls together to get the total result of the roll
+                int rollresult = die1 + die2;
+
+                // Based on the roll result, add one to that position in the array
+                rolls[rollresult - 2] = rolls[rollresult - 2] + 1;
+            }
+
+            // Print the text to explain the results
+            System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS:");
+            System.Console.WriteLine("\nEach '*' represents 1% of the total number of rolls.");
+            System.Console.WriteLine($"\nTotal number of rolls = {number}.\n");
+
+            // Initialize a for loop that will loop for how many times the dice should be rolled
+            for (int iCount = 0;iCount < rolls.Length; iCount++) 
+            {
+                // Initialize an empty string to hold the number of asterisks for each roll result
+                string asterisks = "";
+                
+                // Initialize a variable to hold and calculate the percentage each value was rolled
+                float percentage = ((float)rolls[iCount] / number) * 100;
+
+                // Using a for loop, add the number of asterisks for the amount each number was rolled
+                for (int iCount2 = 0; iCount2 < percentage; iCount2++) 
+                {
+                    asterisks = asterisks + "*";
+                }
+
+                // Write to the console the roll result along with the percentage it was rolled represented by asterisks
+                System.Console.WriteLine($"{iCount + 2}: {asterisks}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using Mission2Assignment;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Initiate a variable for the number of dice rools
+        int RollCount = 0;
+
+        // Create a new instance of the class called 'dice'
+        Class1 dice = new Class1();
+
+        // Print a welcome message to the console window
+        System.Console.WriteLine("Welcome to the dice throwing simulator!\n");
+        System.Console.WriteLine("How many times would you like to roll the die?");
+
+        // Assign the number of dice rolls input by the user to the RollCount Variable
+        RollCount = int.Parse(System.Console.ReadLine());
+
+        // Call the RollDice method from Class1 and pass the RollCount value into it
+        dice.RollDice(RollCount);
+
+        // Say goodbye
+        System.Console.WriteLine("\nThank you for using the Dice Throwing Simulator.  Goodbye!");
+    }
+}
